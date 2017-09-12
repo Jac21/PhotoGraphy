3848ad6b5ebf879fa09b4a25620df2e2918b1a2e
Jeremy Cantu <jcantu521@gmail.com>
2017-09-12 14:39:13 -0500

    Updates

--- src/app/components/photo-grid/photo-grid.component.ts
+++ src/app/components/photo-grid/photo-grid.component.ts
@@ -3,16 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-photo-grid',
   templateUrl: './photo-grid.component.html',
-  styleUrls: ['./photo-grid.component.css']
+  styleUrls: ['./photo-grid.component.css'],
+  moduleId: module.id
 })
 export class PhotoGridComponent implements OnInit {
   constructor() { }
 
   private tiles = [
-    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
-    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+    {
+      src: '../../../assets/images/photography/DSC_4544.JPG', alt: 'One',
+      cols: 3, rows: 2, color: 'lightblue'
+    },
+    {
+      src: '../../../assets/images/photography/DSC_0631.JPG', alt: 'Two',
+      cols: 1, rows: 4, color: 'lightgreen'
+    },
+    {
+      src: '../../../assets/images/photography/DSC_2016.PNG', alt: 'Three',
+      cols: 1, rows: 2, color: 'lightpink'
+    },
+    {
+      src: '../../../assets/images/photography/DSC_1201.JPG', alt: 'Four',
+      cols: 2, rows: 2, color: '#DDBDF1'
+    },
   ];
 
   ngOnInit() {
--- src/app/components/photo/photo.component.ts
+++ src/app/components/photo/photo.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-photo',
   templateUrl: './photo.component.html',
-  styleUrls: ['./photo.component.css']
+  styleUrls: ['./photo.component.css'],
+  moduleId: module.id
 })
 export class PhotoComponent implements OnInit {
 
   constructor() { }
+
+  @Input() photoSrc: string;
+  @Input() photoAlt: string;
 
   ngOnInit() {
   }
