73df4f390cd8587a2071fcf1fa1b07ef035c570a
Jeremy Cantu <jcantu521@gmail.com>
2017-11-06 11:11:30 -0600

    Photo add and updates

--- src/app/components/photo-grid/photo-grid.component.ts
+++ src/app/components/photo-grid/photo-grid.component.ts
@@ -9,8 +9,69 @@ import Macy from "macy";
 })
 export class PhotoGridComponent implements OnInit {
   public macy;
+  private photos;
 
-  constructor() {}
+  constructor() {
+    this.photos = [
+      {
+        photoSrc: "assets/images/photography/DSC_0063.JPG",
+        photoAlt: "Austin"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_0072.JPG",
+        photoAlt: "Austin"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_0087.JPG",
+        photoAlt: "Austin"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_0129.JPG",
+        photoAlt: "Enchanted Rock"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_0335.JPG",
+        photoAlt: "Hamilton Pool"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_0353.JPG",
+        photoAlt: "Hamilton Pool"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_0358.JPG",
+        photoAlt: "Hamilton Pool"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_0396.JPG",
+        photoAlt: "Hamilton Pool"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_0422.JPG",
+        photoAlt: "Hamilton Pool"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_0453.JPG",
+        photoAlt: "Hamilton Pool"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_0631.JPG",
+        photoAlt: "The Mohawk, Austin"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_1009.JPG",
+        photoAlt: "Austin"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_1201.JPG",
+        photoAlt: "Puerto Rico"
+      },
+      {
+        photoSrc: "assets/images/photography/DSC_2016.JPG",
+        photoAlt: "Walnut Creek Park, Austin"
+      },
+      { photoSrc: "assets/images/photography/DSC_4544.JPG", photoAlt: "Spain" }
+    ];
+  }
 
   ngOnInit() {
     this.macy = Macy({
@@ -18,9 +79,9 @@ export class PhotoGridComponent implements OnInit {
       trueOrder: false,
       waitForImages: false,
       margin: 24,
-      columns: 5,
+      columns: 4,
       breakAt: {
-        1200: 5,
+        1200: 4,
         940: 3,
         520: 2,
         400: 1
