a1d97a0be8ef6be880f38501ee96b065bea57b14
Jeremy Cantu <jcantu521@gmail.com>
2024-10-21 18:49:36 -0500

    Material upgrades to support version 15

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatLegacyButtonModule as MatButtonModule } from "@angular/material/legacy-button";
+import { MatLegacyCardModule as MatCardModule } from "@angular/material/legacy-card";
+import { MatLegacyTooltipModule as MatTooltipModule } from "@angular/material/legacy-tooltip";
 
 @NgModule({
   exports: [MatButtonModule, MatCardModule, MatTooltipModule],
