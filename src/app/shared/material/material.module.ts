201262df5cbfe3ab37250441d03426a074dd054d
Jeremy Cantu <jcantu521@gmail.com>
2024-10-21 17:33:24 -0500

    Upgrade Angular Material to version 8

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatTooltipModule
-} from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
 @NgModule({
   exports: [MatButtonModule, MatCardModule, MatTooltipModule],
